733d63e6881bfa37d841c24697709615a40068ec
Omar Atef <20190356@stud.fci-cu.edu.eg>
2021-11-06 20:46:29 +0200

    Removed parser.java (correct file format)

--- src/Terminal.java
+++ src/Terminal.java
@@ -6,6 +6,34 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
+
+class Parser {
+    String commandName;
+    String[] args;
+
+    public Parser(){
+        commandName="";
+        args=null;
+    }
+    //This method will divide the input into commandName and args
+    //where "input" is the string command entered by the user
+    public boolean parse(String input){
+        commandName = input.split(" ")[0];
+        input = input.substring(input.indexOf(" ")+1);
+        args = input.split(" ");
+        return true;
+    }
+
+    public String getCommandName(){
+        return commandName.trim();
+    }
+
+    public String[] getArgs(){
+        return args;
+    }
+}
+
+
 public class Terminal {
     Parser parser;
     String currentDir;
